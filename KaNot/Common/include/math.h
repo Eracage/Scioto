950b5683738621868ba99684a6681d930680ae8d
Cidate <tti12shannak@kamk.fi>
2013-09-19 08:49:40 +0300

    Math file names changed

--- KaNot/Common/include/math.h
+++ KaNot/Common/include/math.h
@@ -12,6 +12,29 @@ namespace Scioto
 		vector(float X, float Y);
 		~vector();
 
+	float getLenght();
+	float getAngle();
+
+	float x,y;
+
+	vector operator -();
+
+	vector operator +(const vector& RightVal);
+	vector operator -(const vector& RightVal);
+
+	vector &operator +=(const vector& RightVal);
+	vector &operator -=(const vector& RightVal);
+
+	vector operator /(float RightVal);
+	vector &operator /=(float RightVal);
+
+	bool operator ==(const vector& RightVal);
+	bool operator !=(const vector& RightVal);
+
+	friend vector operator *(const vector& LeftVal, const float RightVal);
+	friend vector operator *(const float LeftVal, const vector& RightVal);
+
+	friend const vector &operator *=(vector& LeftVal, const float RightVal);
 
 	private:
 
@@ -21,7 +44,16 @@ namespace Scioto
 	class rectangle
 	{
 	public:
+		rectangle();
+		rectangle(float Left, float Top, float Width, float Height);
+		rectangle(vector Position, float Width, float Height);
+		rectangle(vector Position, vector Size);
 
+		bool intersects(rectangle Rectangle);
+		bool contains(vector Position);
+
+		~rectangle();
+		float width, height, left, top;
 
 	private:
 
