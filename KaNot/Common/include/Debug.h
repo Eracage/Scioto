ccea9cfc9c02928495cbd2983ea6de3035f689ca
Eracage <eracage@gmail.com>
2013-09-19 12:24:38 +0300

    updated debug file

--- KaNot/Common/include/Debug.h
+++ KaNot/Common/include/Debug.h
@@ -1,15 +1,27 @@
 #ifdef _WIN32
 #include <Windows.h>
+#include <iostream>
 #define  LOGI(...)  PrintToOutput(false,__VA_ARGS__)
+//Use tostring()
 #define  LOGE(...)  PrintToOutput(true,__VA_ARGS__)
-void PrintToOutput(bool error, const char* message)
+void PrintToOutput(bool error, const char* format, ...)
 {
+	va_list args;
+
+	char message[10240];
+	va_start(args,format);
+	vsprintf(message,format,args);
+	va_end(args);
+
+		
+	OutputDebugStringA("\n");
 	if (error)
 		OutputDebugStringA(" --(ERROR)-- ");
 	OutputDebugStringA(message);
-	OutputDebugStringA("\n");
 }
 #else
+#include <android/log.h>
+#define  LOG_TAG    "libKaNot"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 #endif
