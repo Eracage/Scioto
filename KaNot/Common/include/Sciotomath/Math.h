91bb8e1cef84e7a5515eb590ecef95f8c66e5e1b
Cidate <tti12shannak@kamk.fi>
2013-10-17 08:36:51 +0300

    Vector files minimized. Added to math.h

--- KaNot/Common/include/Sciotomath/Math.h
+++ KaNot/Common/include/Sciotomath/Math.h
@@ -3,9 +3,8 @@
 #define PI 3.14159265358979323846264
 
 #include <Sciotomath\Rectangle.h>
-#include <Sciotomath\Vector2.h>
-#include <Sciotomath\Vector3.h>
-#include <Sciotomath\Vector4.h>
+#include <Sciotomath\Vector.h>
+
 
 
 #endif
--- KaNot/Common/include/Sciotomath/Vector.h
+++ KaNot/Common/include/Sciotomath/Vector.h
@@ -1,8 +1,8 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-#include <Vector2.h>
-#include <Vector3.h>
-#include <Vector4.h>
+#include <Sciotomath/Vector2.h>
+#include <Sciotomath/Vector3.h>
+#include <Sciotomath/Vector4.h>
 
 #endif
